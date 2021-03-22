f68f8b689a2e5c0f5f7c39259434014fd5308d55
Mula Njira <ginjira@microsoft.com>
2021-03-22 15:55:54 +0300

    Parse rsdl.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-// Something
+import { parse } from 'rsdl-js';
+
+document.addEventListener('DOMContentLoaded', function () {
+  const convertButton = document.getElementById('convertButton');
+  const rsdlTextArea = document.getElementById('rsdlTextArea');
+  const mermaidTextArea = document.getElementById('mermaidTextArea');
+
+  const diagramContainer = document.getElementById('diagramContainer');
+
+  convertButton.addEventListener('click', function () {
+    try {
+      const source = rsdlTextArea.value;
+      const { rsdljs, errors } = convertRsdl(source);
+
+      if (errors) {
+        errors.map((error) => console.error(error));
+        return;
+      }
+
+    
+      mermaidTextArea.value = JSON.stringify(rsdljs, null, 2);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+});
+
+function convertRsdl(source) {
+  try {
+    const json = parse(source, (_) => (_) => '');
+    if (json.$$errors) {
+      return { errors: json.$$errors };
+    }
+
+    return { rsdljs: json };
+  } catch (e) {
+    return { errors: [e] };
+  }
+}
